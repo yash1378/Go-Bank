599b3eacdf67581d259c3e3d228a1d1e84df25d6
yash <slayycasm997@gmail.com>
2024-12-07 15:53:04 +0530

    Video 4 finished

--- api.go
+++ api.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"sync"
 
+	jwt "github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
 )
 
@@ -29,10 +31,63 @@ func NewAPIServer(listenAddr string, store Storage) *APIServer {
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
-	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID))
+	router.HandleFunc("/account/{id}", withJWTAuth(makeHTTPHandleFunc(s.handleGetAccountByID), s.store))
 	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
 	log.Println("JSON API server running on port: ", s.listenAddr)
 	http.ListenAndServe(s.listenAddr, router)
+}
+
+func withJWTAuth(handlerfunc http.HandlerFunc, store Storage) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling JWT Auth middleware")
+		tokenString := r.Header.Get("x-jwt-token")
+		token, err := validateJWT(tokenString)
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, apiError{Error: "invalid-token"})
+			return
+		}
+
+		if !token.Valid {
+			WriteJSON(w, http.StatusForbidden, apiError{Error: "invalid-token"})
+			return
+		}
+
+		// id := mux.Vars(r)["id"]
+		// userID := Str_to_Int(id)
+
+		// account,err :=
+
+		claims := token.Claims.(jwt.MapClaims)
+		fmt.Println(claims)
+		handlerfunc(w, r)
+	}
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	// Parse and validate the token
+	secretKey := os.Getenv("JWT_SECRET")
+
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validate the algorithm used
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+
+	// // Handle parsing and validation errors
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to validate token: %v", err)
+	// }
+
+	// // Check if the token is valid
+	// if !token.Valid {
+	// 	return nil, fmt.Errorf("invalid token")
+	// }
+
+	// // If all checks pass, return the valid token
+	// return token, nil
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
@@ -95,16 +150,34 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	}
 
 	account := NewAccount(Obj.FirstName, Obj.LastName)
-	fmt.Println(1)
+	// fmt.Println(1)
 	account.ID = a
 	a = a + 1
 	if err := s.store.CreateAccount(account); err != nil {
-		fmt.Println(2)
+		// fmt.Println(2)
 		return err
 	}
-	fmt.Println(3)
+	// fmt.Println(3)
+
+	tokenString, err := createJWT(account)
+	if err != nil {
+		return err
+	}
+	fmt.Println("JWT Token: ", tokenString)
 
 	return WriteJSON(w, http.StatusOK, account)
+
+}
+
+func createJWT(account *Account) (string, error) {
+	claims := &jwt.MapClaims{
+		// Also fixed dates can be used for the NumericDate
+		"expiresAt":     15000,
+		"accountNumber": account.Number,
+	}
+	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
 
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yash1378/gobank
 go 1.22.2
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
